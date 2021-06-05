f9f0b68dea01c563f6dd92b9f3ccac09ff7a2600
Harish Nishad <harish.nishad0775@gmail.com>
2021-06-05 23:52:08 +0530

    add file

--- Z_CodeRichi/index.js
+++ Z_CodeRichi/index.js
@@ -12,7 +12,7 @@ const passportJWT = require('./config/passport-jwt-strategy');
 const passportGoogle = require('./config/passport-google-oauth2-strategy');
 
 const MongoStore = require('connect-mongo')(session);           
-const sassMiddleware = require('node-sass-middleware');       
+const sassMiddleware = require('node-sass-middleware'); 
 const flash = require('connect-flash');                
 const customMware = require('./config/middleware');            
                                                                             
