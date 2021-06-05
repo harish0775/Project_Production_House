a76eb1f3b2593b493aee01dfe8aecaf29910e3d4
Harish Nishad <harish.nishad0775@gmail.com>
2021-06-05 23:51:23 +0530

    add file

--- Z_CodeRichi/index.js
+++ Z_CodeRichi/index.js
@@ -15,7 +15,7 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');       
 const flash = require('connect-flash');                
 const customMware = require('./config/middleware');            
-                                                                          
+                                                                            
 app.use(sassMiddleware({                  
     src: './assets/scss',                    
     dest: './assets/css',                     
